e9a6b6b877727a184a1e9fac7e5f46aa87654d16
naka-c1024 <ny42800824@gmail.com>
2022-09-13 09:52:27 +0900

    リファクタリング

--- ex00/convert/mypkg/convert.go
+++ ex00/convert/mypkg/convert.go
@@ -15,53 +15,55 @@ import (
 // ErrMsg はエラーメッセージを表すユーザー定義型です。
 type ErrMsg string
 
-func isDir(directory string) bool {
-	fInfo, _ := os.Stat(directory)
-	if fInfo == nil {
-		fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", directory)
-		os.Exit(1)
-	}
-	if fInfo.IsDir() == false {
-		return false
-	}
-	return true
-}
-
-func isPng(str string) bool {
-	if ext := filepath.Ext(str); ext == ".png" {
+// IsPng は.pngファイルかどうかbool値で返す関数です。
+func IsPng(path string) bool {
+	if ext := filepath.Ext(path); ext == ".png" {
 		return true
 	} else {
 		return false
 	}
 }
 
-func checkError(err error, msg ErrMsg) {
+// CheckError はerrorが生じた際にその内容を出力しプログラムを終了する関数です。
+func CheckError(err error, msg ErrMsg) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v: %v\n", msg, err)
+		fmt.Fprintf(os.Stderr, "error: %v: %v\n", msg, err)
 		os.Exit(1)
 	}
 }
 
-func jpgToPng(path string) {
+// JpgToPng は.jpgファイルから.pngファイルに変換する関数です。
+func JpgToPng(path string) {
 	file, err := os.Open(path)
-	checkError(err, "open")
+	CheckError(err, "open")
 	defer file.Close()
 
 	img, _, err := image.Decode(file)
-	checkError(err, "decode")
+	CheckError(err, "decode")
 
 	png_file := strings.Replace(path, "jpg", "png", -1)
 	out, err := os.Create(png_file)
-	checkError(err, "create")
+	CheckError(err, "create")
 	defer out.Close()
 
 	png.Encode(out, img)
 }
 
-func myWalk(dirname string) {
-	if isDir(dirname) == false {
-		fmt.Fprintf(os.Stderr, "error: %s is not directory\n", dirname)
-		os.Exit(0)
+// TrimSpaceLeft はエラーメッセージにおいて不要なスペースから左部分を除く関数です。
+func TrimSpaceLeft(err error) string {
+	str := err.Error()
+	spaceIndex := strings.Index(str, " ")
+	if spaceIndex == -1 {
+		return str
+	}
+	return str[spaceIndex+1:]
+}
+
+// FindJpg は.jpgファイルを探す関数です。
+func FindJpg(dirname string) {
+	if _, err := os.Stat(dirname); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", TrimSpaceLeft(err))
+		os.Exit(1)
 	}
 	err := filepath.Walk(dirname,
 		func(path string, info os.FileInfo, err error) error {
@@ -69,19 +71,19 @@ func myWalk(dirname string) {
 				return err
 			}
 			if filepath.Ext(path) == ".jpg" {
-				jpgToPng(path)
-			} else if isDir(path) == false && isPng(path) == false {
+				JpgToPng(path)
+			} else if info.IsDir() == false && IsPng(path) == false {
 				fmt.Fprintf(os.Stderr, "error: %s is not a valid file\n", path)
 			}
 			return nil
 		})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", TrimSpaceLeft(err))
 		os.Exit(1)
 	}
 }
 
-// Convert は.jpgファイルを.pngファイルに変換する関数です。
+// Convert はconvertのmainとなる関数です。
 func Convert() {
 	flag.Parse()
 	if dirname := flag.Arg(0); dirname == "" {
@@ -91,6 +93,6 @@ func Convert() {
 		fmt.Fprintf(os.Stderr, "error: multiple arguments\n")
 		os.Exit(0)
 	} else {
-		myWalk(dirname)
+		FindJpg(dirname)
 	}
 }
